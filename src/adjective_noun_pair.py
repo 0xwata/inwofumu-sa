7134586bb8d743f2e3972e2af8c6d2c39d15c498
watarushi <watarutakamine@gmail.com>
2021-06-10 20:00:49 +0900

    不具合修正

--- src/adjective_noun_pair.py
+++ src/adjective_noun_pair.py
@@ -1,19 +1,30 @@
 import random
 import sentiment_polarity
 
-
+BASE_URL = "../data/random-adjectives-nouns/"
 class AdjectiveNounPairSelector:
-    def __init__(self, word_sentiment: str):
+    def __init__(self):
         # "positive" or "negative"
-        self.word_sentiment = word_sentiment
+        # self.word_sentiment = word_sentiment
+
         # import adjectives list
-        with open("EnglishList/english-adjectives.txt") as f:
+        with open( BASE_URL + "english-adjectives.txt") as f:
             self.adjectives = f.readlines()
+        print("adjectiveリストの読み込み完了")
         # import nouns list
-        with open("EnglishList/english-nouns.txt") as f:
+        with open( BASE_URL + "english-nouns.txt") as f:
             self.nouns = f.readlines()
+        print("nounリストの読み込み完了")
 
     def fetch_random_adjective(self) -> str:
+        adj_index = random.randrange(len(self.adjectives))
+        return self.adjectives[adj_index]
+
+    def fetch_random_noun(self) -> str:
+        noun_index = random.randrange(len(self.nouns))
+        return self.nouns[noun_index]
+
+    def fetch_random_adjective_with_sentiment(self) -> str:
         sentiment_score = 0
         if self.word_sentiment == "positive":
             sentiment_score_threshold = 0.8
@@ -29,9 +40,5 @@ class AdjectiveNounPairSelector:
                 )
         return self.adjectives[adj_index]
 
-    def fetch_random_noun(self) -> str:
-        noun_index = random.randrange(len(self.nouns))
-        return self.nouns[noun_index]
-
-    def __fetch_sentiment_polarity_score(self, word: str) -> float:
+    def fetch_sentiment_polarity_score(self, word: str) -> float:
         return sentiment_polarity.SentimentPolarity.fetch_score(word)
