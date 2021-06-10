63d7dc27119b196f4beb4809c6603d185ac19a25
watarushi <watarutakamine@gmail.com>
2021-06-10 20:02:08 +0900

    一通りアウトプット出すところまでスクリプト追加

--- src/main.py
+++ src/main.py
@@ -1,17 +1,17 @@
-from src.adjective_noun_pair import AdjectiveNounPairSelector
-from src.
+from adjective_noun_pair import AdjectiveNounPairSelector
+from translate import Translate
+from ipa_match_word_searcher import IpaMatchWordSearcher
+import json
 
 def main():
+
+    output = []
+    N = 100
+
     """
     名詞・形容詞のランダム取得クラスのインタンス化
     """
     adjective_noun_pair_selector = AdjectiveNounPairSelector()
-
-    """
-    形容詞・名詞の取得
-    """
-    adjective: str = adjective_noun_pair_selector.fetch_random_adjective()
-    noun: str = adjective_noun_pair_selector.fetch_random_noun()
 
     """
     翻訳クラスのインスタンス化
@@ -19,55 +19,57 @@ def main():
     translate = Translate()
 
     """
-    形容詞・名詞の翻訳処理
-    """
-    adjectives_translated = translate.translate_all_language(adjective)
-    nouns_translated = translate.translate_all_language(noun)
-
-
-
-    """
-    形容詞・名詞のIPA変換→マッチング処理
+    形容詞・名詞のIPA変換→マッチング処理のインスタンス化
     """
     ipa_converter = IpaMatchWordSearcher()
-    ipa_converter.search(adjectives_translated)
 
-    """
-    マッチング済み単語の翻訳処理
-    """
+    for i in range(N):
 
-    """
-    マッチング済み単語の品詞推定処理
-    """
+        """
+        形容詞・名詞の取得
+        """
+        request_adjective = adjective_noun_pair_selector.fetch_random_adjective()
+        request_noun = adjective_noun_pair_selector.fetch_random_noun()
 
-    """
-    最終的なアウトプット
-    [
-        {
-            request: {
-                "adjective": {
-                    "word":
-                    "lang":
-                },
-                "noun": {
-                    "word":
-                    "lang":
-                }
-            },
-            response: {
-                "adjective": {
-                    "word":
-                    "lang":
-                },
-                "noun": {
-                    "word":
-                    "lang":
-                }
-            }
-        }, ...
-    ]
-    """
+        """
+        形容詞・名詞の翻訳処理
+        指定した７言語に翻訳
+        """
+        print()
+        print("request-adjective:"+request_adjective)
+        print("request-noun:"+request_noun)
 
+        print("形容詞の翻訳開始")
+        request_adjectives_translated = translate.translate_all_language(request_adjective)
+
+        print("名詞の翻訳開始")
+        request_nouns_translated = translate.translate_all_language(request_noun)
+
+        """
+        形容詞・名詞のIPA変換→マッチング処理
+        """
+        request_adjective, response_adjective  = ipa_converter.execute(words = request_adjectives_translated, pos="JJ")
+        request_noun, response_noun  = ipa_converter.execute(words = request_adjectives_translated, pos="NN")
+
+        request_dict = {
+            "adjective" : request_adjective,
+            "noun" : request_noun
+        }
+        response_dict = {
+            "adjective" : response_adjective,
+            "noun": response_noun
+        }
+
+        request_response_dict = {
+            "request": request_dict,
+            "response": response_dict
+        }
+
+    # jsonに書き込み
+    output.append(request_response_dict)
+    file_output = open('output.json','w')
+    json.dump(output,file_output,indent=4) #ensure_ascii=False
+    file_output.close()
 
 if __name__ == "__main__":
     main()
