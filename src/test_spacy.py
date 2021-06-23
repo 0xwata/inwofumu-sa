27a51ab4f3f16b22a9a79fcc8bff21629e9176c9
watarushi <watarutakamine@gmail.com>
2021-06-24 02:57:56 +0900

    update spacy code

--- src/test_spacy.py
+++ src/test_spacy.py
@@ -4,36 +4,53 @@ import spacy
 from spacy.symbols import ADJ, NOUN, PROPN, PRON, VERB
 import numpy as np
 
-# 参考 https://yu-nix.com/blog/2021/3/3/spacy-pos-list/
-convert_dic = {
-    'ADJ': "adjective",
-    'NOUN': "noun",
-    'PROPN': "noun",
-    'PRON': "noun",
-    'VERB': "verb",
-
-}
 spacy_symbol_noun_list = [NOUN, PROPN, PRON]
-df = pd.read_csv("../output/final/3447.csv", sep=',')
 
-nlp = spacy.load("en_core_web_sm")
+nlp_en = spacy.load("en_core_web_sm")
+nlp_zh = spacy.load("zh_core_web_sm")
+nlp_es = spacy.load("es_core_news_sm")
+nlp_de = spacy.load("de_core_news_sm")
+nlp_ja = spacy.load("ja_core_news_sm")
+nlp_ro = spacy.load("ro_core_news_sm")
+
+lang_converter_dic_for_spacy = {
+    "de":nlp_de,#
+    "en":nlp_en,#
+    "es":nlp_es,#
+    "ja":nlp_ja,#
+    "ro":nlp_ro,#
+    "zh-cn":nlp_zh,#
+    # "eo":"eo",
+    # "id":nlp_id,
+    # "ko":nlp_ko,
+}
+
+df = pd.read_csv("../output/final/3447.csv", sep=',')
 
 print(ADJ, NOUN, PROPN, PRON, VERB)
 word_pos_list = []
 for i, row in df.iterrows():
     pos_spcy = "other"
-    print(row.word_en)
-    print(type(row.word_en))
-    if row.word_en is np.nan:
-        word_pos_list.append("nan")
-        continue
-    doc = nlp(row.word_en)
+
+    if row.word_lang in lang_converter_dic_for_spacy.keys():
+        nlp = lang_converter_dic_for_spacy[row.word_lang]
+        if row.word is np.nan:
+            word_pos_list.append("nan")
+            continue
+        target_word = row.word
+    else:
+        nlp = nlp_en
+        if row.word_en is np.nan:
+            word_pos_list.append("nan")
+            continue
+        target_word = row.word_en
+
+    doc = nlp(target_word)
     for tok in doc:
         print(tok.text)
         if tok.pos == ADJ:
             pos_spcy = "adjective"
         elif tok.pos == VERB:
-            print("verb")
             pos_spcy = "verb"
         elif tok.pos in spacy_symbol_noun_list:
             pos_spcy = "noun"
