fe8879ae0169b2d46a3fd4c437c7fe30ddc3718d
0xwata <watarutakamine@gmail.com>
2022-11-06 00:07:18 +0900

    データ修正

--- v2/src/util/fix_output_column.py
+++ v2/src/util/fix_output_column.py
@@ -12,16 +12,16 @@ def main():
                "rhyme_type", "match_ipa_rhyme", "pos", "syllable"
                ]
 
-  df = pd.read_csv("../output/rhyme_pair/v2/b9c16b9a-5a58-11ed-acf9-00163ec0635e.csv")
-  tmp_word_lang = df["word_lang"] ## 実質 word_ipa
-  tmp_word_ipa = df["word_ipa"] ## 実質 word_ipa_rhyme
-  tmp_word_ipa_rhyme = df["word_ipa_rhyme"] ## 実質 lang
+  df = pd.read_csv("../output/rhyme_pair/v2/b857717c-5a4a-11ed-ae8b-acde48001122.csv")
+  tmp_word_lang = df["pair_word_lang"] ## 実質 word_ipa
+  tmp_word_ipa = df["pair_word_ipa"] ## 実質 word_ipa_rhyme
+  tmp_word_ipa_rhyme = df["pair_word_ipa_rhyme"] ## 実質 lang
 
-  df["word_lang"] = tmp_word_ipa_rhyme
-  df["word_ipa"] = tmp_word_lang
-  df["word_ipa_rhyme"] = tmp_word_ipa
+  df["pair_word_lang"] = tmp_word_ipa_rhyme
+  df["pair_word_ipa"] = tmp_word_lang
+  df["pair_word_ipa_rhyme"] = tmp_word_ipa
 
-  df.to_csv("../output/rhyme_pair/v2/fix-b9c16b9a-5a58-11ed-acf9-00163ec0635e.csv", index=False)
+  df.to_csv("../output/rhyme_pair/v2/fix-b857717c-5a4a-11ed-ae8b-acde48001122.csv", index=False)
 
 
 if __name__ == "__main__":
